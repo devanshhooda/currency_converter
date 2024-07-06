328e64e89baeb3ecb14af88a5da3689de14752ed
devanshhooda <devanshstan17@gmail.com>
2024-07-06 20:31:06 +0530

    Utilising several hooks and components in main app

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,42 @@
+import { useState } from 'react'
 import './App.css'
+import { InputBox } from './components'
+import useCurrencyInfo from './hooks/useCurrencyInfo';
 
 function App() {
+  const [fromBoxSelectedCurrency, setFromBoxCurrency] = useState("usd");
+  const [fromBoxAmount, setFromBoxAmount] = useState(0);
+
+  const [toBoxSelectedCurrency, setToBoxCurrency] = useState("inr");
+  const [toBoxAmount, setToBoxAmount] = useState(0);
+
+  const currencyData = useCurrencyInfo(fromBoxSelectedCurrency);
+
   return (
     <>
       <h1 className='bg-black text-white text-3xl'>Currency Converter</h1>
+
+      <InputBox
+        inputLabel='From'
+        selectedCurrency={fromBoxSelectedCurrency}
+        amount={fromBoxAmount}
+        currencyOptionsList={[]}
+        onCurrencyChange={(currency) => setFromBoxCurrency(currency)}
+        onAmountChange={(amount) => setFromBoxAmount(amount)} />
+
+      <InputBox
+        inputLabel='To'
+        selectedCurrency={toBoxSelectedCurrency}
+        amount={toBoxAmount}
+        currencyOptionsList={[]}
+        onCurrencyChange={(currency) => setToBoxCurrency(currency)}
+        onAmountChange={(amount) => setToBoxAmount(amount)} />
+
+      <button>
+        Click to convert
+      </button>
     </>
   )
 }
 
-export default App
+export default App;
