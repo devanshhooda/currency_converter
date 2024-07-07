011439b83a94d1df065ddf4869c3be834bd16dfd
devanshhooda <devanshstan17@gmail.com>
2024-07-07 20:19:56 +0530

    Final touches

--- src/hooks/useCurrencyInfo.ts
+++ src/hooks/useCurrencyInfo.ts
@@ -2,16 +2,16 @@ import { useEffect, useState } from "react";
 
 let currencyInfoURL = 'https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies';
 
-const useCurrencyInfo = (currency: string) => {
+const useCurrencyInfo = (currency: string): Record<string, any> => {
     const [currencyData, setCurrencyData] = useState({});
 
     useEffect(() => {
         fetch(`${currencyInfoURL}/${currency}.json`)
             .then((res) => res.json())
-            .then((resJson) => setCurrencyData(resJson[currency]));
+            .then((resJson: Record<string, any>) => setCurrencyData(resJson[currency]));
         ;
 
-        console.log(`Updated currencyData: ${currencyData}`)
+        // console.log(`Updated currencyData: ${currencyData}`)
     }, [currency]);
 
     return currencyData;
